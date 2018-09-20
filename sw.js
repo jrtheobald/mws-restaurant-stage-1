2b9bc20fb53c8ac320e4570807f5b9ba8a47c3bc
James R Theobald <jrtheobald_git@icloud.com>
2018-09-19 23:20:33 -0400

    Modify sw.js with fetch from The offline cookbook

--- sw.js
+++ sw.js
@@ -26,12 +26,26 @@ self.addEventListener('install', function(event) {
     );
 });
 
+// Code taken from https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/#on-network-response
+
+// The Offline Cookbook by Jake Archibald
+// Stale-while-revalidate
+// Web Fundamentals
+// This code was chosen for its ability to cache the map and load the cached map when offline.
+// Files are only cached when the user clicks into content enabling the fetch to cache data from the network.
+// Content not visited while on the network does not cache, and therefore will not load while offline.
+
+// TODO: Review other types of service worker fetch listeners
 self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request)
-    .then(function(response) {
-        if (response) {
-            return response;
-        }
-        return fetch(event.request);
-    }));
-});
+    event.respondWith(
+      caches.open('filesCached').then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          var fetchPromise = fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+          return response || fetchPromise;
+        })
+      })
+    );
+  });
